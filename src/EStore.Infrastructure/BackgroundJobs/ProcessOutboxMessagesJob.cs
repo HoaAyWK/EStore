8dac1b97363967e2fb86997866adc39dd65e50ae
Hoa Vy <vycoi1511@gmail.com>
2024-02-03 11:50:18 +0700

    add retry for ProcessOutboxMessage

--- src/EStore.Infrastructure/BackgroundJobs/ProcessOutboxMessagesJob.cs
+++ src/EStore.Infrastructure/BackgroundJobs/ProcessOutboxMessagesJob.cs
@@ -4,6 +4,8 @@ using EStore.Infrastructure.Persistence.Outbox;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using Polly;
+using Polly.Retry;
 using Quartz;
 
 namespace EStore.Infrastructure.BackgroundJobs;
@@ -42,7 +44,20 @@ public class ProcessOutboxMessagesJob : IJob
                 continue;
             }
 
-            await _publisher.Publish(domainEvent, context.CancellationToken);
+            var pipeline = new ResiliencePipelineBuilder()
+                .AddRetry(new RetryStrategyOptions()
+                {
+                    MaxRetryAttempts = 3,
+                    BackoffType = DelayBackoffType.Constant,
+                    Delay = TimeSpan.Zero,
+                    ShouldHandle = new PredicateBuilder()
+                        .Handle<Exception>()
+                })
+                .Build();
+
+            await pipeline.ExecuteAsync(async (token) =>
+                await _publisher.Publish(domainEvent, token));
+            
             outboxMessage.ProcessedOnUtc = DateTime.UtcNow;
             await _dbContext.SaveChangesAsync();
         }
